a16a2803953c7a6e2fc4f8ec5baeeffdc6c3cf5f
Phongnguyen <nguyendacphong09@gmail.com>
2024-07-27 01:23:52 +0700

    fix environment available

--- Backend/api/api/settings/base.py
+++ Backend/api/api/settings/base.py
@@ -13,13 +13,17 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6zyz&%b_pk#n7zl+s54p^893fqwj&m)s4xo1$l78*xtb0m)q$!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
